1fa8559b9b2c2c9955ed03a17022cb8f84e50d06
johnbuttigieg <john.buttigieg@my.jcu.edu.au>
2017-10-30 09:03:45 +1000

    Few Minor changes
    minor changes

--- src/MulticastRead.java
+++ src/MulticastRead.java
@@ -1,2 +1,31 @@
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class MulticastRead {
+    public static void main(String[] args) throws Exception {
+
+        InetAddress address = InetAddress.getByName("228.5.6.7");
+        MulticastSocket socket = new MulticastSocket(55555);
+
+        socket.joinGroup(address);
+
+        byte[] messageBuffer = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(messageBuffer, 1024);
+
+        //Statement that waits until something is sent...
+        socket.receive(receivePacket);
+
+        String recievetext = new String(messageBuffer);
+
+        System.out.println(recievetext);
+
+
+
+        socket.close();
+        socket.leaveGroup(address);
+
+    }
+
+
 }
--- src/MulticastSend.java
+++ src/MulticastSend.java
@@ -1,2 +1,30 @@
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class MulticastSend {
+    public static void main(String[] args) throws Exception {
+
+        String sendtext = "Message sent to be received";
+
+        InetAddress address = InetAddress.getByName("228.5.6.7");
+        MulticastSocket socket = new MulticastSocket(55555);
+
+        InetAddress localIP = InetAddress.getLocalHost();
+
+        socket.joinGroup(address);
+
+        DatagramPacket packet = new DatagramPacket(sendtext.getBytes(), sendtext.length(), address, 55555);
+
+
+        socket.send(packet);
+
+        socket.leaveGroup(address);
+        socket.close();
+
+
+
+    }
+
+
 }
